cec07784065c278d3bab6db11be97e7ec7f8df09
SilvioC2C <silvio.gregorini@camptocamp.com>
2022-10-05 14:00:01 +0200

    [IMP] rest_log: handle ``odoo.http.Response`` objects in logs
    Some endpoints will not return a dict as expected by the ``_dispatch_with_db_logging`` method (and the other methods called within), but an object of type ``odoo.http.Response``.
    When that happens, the instance fails to handle it while creating logs (ie: tries to call ``json.dumps()`` on such object, which is not allowed).
    This patch should fix the issue.

--- rest_log/components/service.py
+++ rest_log/components/service.py
@@ -9,7 +9,7 @@ import traceback
 from werkzeug.urls import url_encode, url_join
 
 from odoo import exceptions, registry
-from odoo.http import request
+from odoo.http import Response, request
 
 from odoo.addons.base_rest.http import JSONEncoder
 from odoo.addons.component.core import AbstractComponent
@@ -68,7 +68,7 @@ class BaseRESTService(AbstractComponent):
         log_entry = self._log_call_in_db(
             self.env, request, method_name, *args, params=params, result=result
         )
-        if log_entry:
+        if log_entry and isinstance(result, dict):
             log_entry_url = self._get_log_entry_url(log_entry)
             result["log_entry_url"] = log_entry_url
         return result
@@ -123,6 +123,12 @@ class BaseRESTService(AbstractComponent):
             params = dict(params or {}, args=args)
 
         result = kw.get("result")
+        if isinstance(result, Response):
+            status_code = result.status_code
+            result = {"status_code": status_code}
+            state = "success" if status_code in range(200, 300) else "failed"
+        else:
+            state = "success" if result else "failed"
         error = kw.get("traceback")
         orig_exception = kw.get("orig_exception")
         exception_name = None
@@ -142,7 +148,7 @@ class BaseRESTService(AbstractComponent):
             "error": error,
             "exception_name": exception_name,
             "exception_message": exception_message,
-            "state": "success" if result else "failed",
+            "state": state,
         }
 
     def _log_call_in_db(self, env, _request, method_name, *args, params=None, **kw):
