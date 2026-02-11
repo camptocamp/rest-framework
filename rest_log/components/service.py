a92da5437b0e9c7d29a9c41d0d4bc66ea573dba7
Simone Orsi <simahawk@gmail.com>
2026-02-11 10:36:29 +0100

    fixup! fixup! [REF] rest_log: Add hooks to read configuration from system params

--- rest_log/components/service.py
+++ rest_log/components/service.py
@@ -14,6 +14,7 @@ from odoo import exceptions
 from odoo.http import Response, request
 from odoo.modules.registry import Registry
 from odoo.service.model import PG_CONCURRENCY_ERRORS_TO_RETRY
+from odoo.tools.profiler import Profiler
 
 from odoo.addons.base_rest.http import JSONEncoder
 from odoo.addons.component.core import AbstractComponent
@@ -47,10 +48,13 @@ class BaseRESTService(AbstractComponent):
                 args,
                 params,
             )
-        if self._start_profiling(method_name):
-            self.env["ir.profile"].set_profiling(profile=True)
         if not self._db_logging_active(method_name):
             return super().dispatch(method_name, *args, params=params)
+        if self._start_profiling(method_name):
+            with Profiler(
+                description=f"REST LOG {self._collection}.{self._usage}.{method_name}",
+            ):
+                return self._dispatch_with_db_logging(method_name, *args, params=params)
         return self._dispatch_with_db_logging(method_name, *args, params=params)
 
     def _dispatch_with_db_logging(self, method_name, *args, params=None):
@@ -262,21 +266,9 @@ class BaseRESTService(AbstractComponent):
             and self.env.uid == profiling_uid
         )
         if res:
-            profiling_duration = 10
-            try:
-                profiling_duration = int(
-                    self.env["ir.config_parameter"]
-                    .sudo()
-                    .get_param("rest.log.profiling.duration")
-                )
-            except ValueError:
-                _logger.warning(
-                    "System parameter rest.log.profiling.duration is not defined. Profiling will be active for the next 10 minutes"
-                )
-            profiling_enabled_until = fields.Datetime.now() + relativedelta(
-                minutes=profiling_duration
-            )
-            self.env["ir.config_parameter"].sudo().set_param(
-                "base.profiling_enabled_until", profiling_enabled_until
+            _logger.info(
+                "Profiling enabled for uid=%s %s",
+                profiling_uid,
+                f"{self._collection}.{self._usage}.{method_name}",
             )
         return res
